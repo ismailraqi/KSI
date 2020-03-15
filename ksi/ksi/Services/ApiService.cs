922759c625b17ce4d58345394aaaa428d718028c
walrusdev <raqiking@gmail.com>
2020-03-15 23:37:45 +0000

    init Commit

---
+++ ksi/ksi/Services/ApiService.cs
@@ -0,0 +1,40 @@
+﻿using ksi.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ksi.Services
+{
+    class ApiService
+    {
+        public async Task<bool> RegisterAsync(string email, string password, string confirmPassword)
+        {
+            var client = new HttpClient();
+            var model = new RegisterBindingModel
+            {
+                Email=email,
+                Password=password,
+                ConfirmPassword=confirmPassword
+            };
+            var json = JsonConvert.SerializeObject(model);
+            HttpContent content = new StringContent(json);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var response = await client.PostAsync("http://192.168.43.21:45455/api/Account/Register", content);
+            return response.IsSuccessStatusCode;
+        }
+
+        //public async Task LoginAsync(string userName, string password)
+        //{
+        //    var keyValues = new List<KeyValuePair<string, string>>
+        //    {
+        //        new KeyValuePair<string, string>("username",userName),
+        //        new KeyValuePair<string, string>("password",password),
+        //        new KeyValuePair<string, string>("grant_type","password")
+        //    };
+        //}
+    }
+}
---
+++ ksi/ksi/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,36 @@
+﻿using ksi.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ksi.ViewModels
+{
+    class RegisterViewModel
+    {
+        ApiService apiService = new ApiService();
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Message { get; set; }
+        public ICommand RegisterCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    var isSuccess = await apiService.RegisterAsync(Email, Password, ConfirmPassword);
+                    if (isSuccess)
+                    {
+                        Message = "Register Success";
+                    }
+                    else
+                    {
+                        Message = "Try Again";
+                    }
+                });
+            }
+        }
+    }
+}
